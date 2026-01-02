78ae15bc5706099c9ec2f354d0df7c7887e2195a
citmandigitaljobs <catherine.itman@gmail.com>
2026-01-02 21:51:28 +1100

    Create main.js

---
+++ main.js
@@ -0,0 +1,71 @@
+function myFunctionOne() {
+  var x = document.getElementById("clickPreviewOne");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionTwo() {
+  var x = document.getElementById("clickPreviewTwo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionThree() {
+  var x = document.getElementById("clickPreviewThree");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionFour() {
+  var x = document.getElementById("clickPreviewFour");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionFive() {
+  var x = document.getElementById("clickPreviewFive");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionSix() {
+  var x = document.getElementById("clickPreviewSix");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionSeven() {
+  var x = document.getElementById("clickPreviewSeven");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionEight() {
+  var x = document.getElementById("clickPreviewEight");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
